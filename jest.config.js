4cea2ceef9d638fee69f865ddd03220c23ac3a85
matijs <matijs@gmail.com>
2019-12-10 19:58:35 +0100

    Setup jest, ts-jest and enzyme
    Command used:
    npm install --save-dev jest ts-jest enzyme enzyme-adapter-react-16 \
      enzyme-to-json @types/jest @types/enzyme @types/enzyme-adapter-react-16
    npx jest --init
    
    Install Jest and Enzyme and @types definitions and configure Jest.
    
    Coverage ends up in ./__coverage__, enzyme-to-json is used to serialize
    snapshots, ts-jest is used for transforming TypeScript

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+    clearMocks: true,
+    coverageDirectory: '__coverage__',
+    preset: 'ts-jest',
+    rootDir: './src',
+    setupFilesAfterEnv: ['<rootDir>/__tests__/jest.setup.ts'],
+    snapshotSerializers: ['enzyme-to-json/serializer'],
+    testPathIgnorePatterns: [
+        '/node_modules/',
+        'jest.setup.ts',
+    ],
+};
