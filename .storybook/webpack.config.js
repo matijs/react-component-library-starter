0c2ac0d1231aee42d1be1e8a190974ccb2baf367
matijs <matijs@gmail.com>
2019-12-11 20:34:24 +0100

    Prepare Storybook
    Add loaders for SCSS

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,12 +1,20 @@
+const sass = require('sass');
+
 module.exports = ({ config }) => {
-    config.module.rules.push({
-        test: /\.(ts|tsx)$/,
-        use: [
-            {
-                loader: 'awesome-typescript-loader',
-            },
-        ],
-    });
+    config.module.rules.push(
+        {
+            test: /\.(ts|tsx)$/,
+            use: 'awesome-typescript-loader',
+        },
+        {
+            test: /\.scss$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                { loader: 'sass-loader', options: { implementation: sass } },
+            ],
+        },
+    );
     config.resolve.extensions.push('.ts', '.tsx');
     return config;
 };
